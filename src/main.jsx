ce3f625b668393edde321eedd81cae2e211a8451
Adam KASSIS <adam.kassis2005@gmail.com>
2025-09-04 12:23:15 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store.js';
+import { BrowserRouter } from 'react-router-dom';  // <- added this
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/e-plantShopping/">   {/* <- set basename */}
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
