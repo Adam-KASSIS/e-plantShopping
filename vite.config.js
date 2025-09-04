a11681f0ec98f9e5e9e31a33f9313523f694dc82
Adam KASSIS <adam.kassis2005@gmail.com>
2025-09-04 12:12:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/e-plantShopping/",  // <-- must match your old repo name
+  base: "/e-plantShopping/",  
   plugins: [react()],
 })
